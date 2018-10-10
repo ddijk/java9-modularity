91c7584094e6803a0e85f56e9031a1b48201099e
ddijk <dick@dijkrosoft.nl>
2018-10-10 20:11:13 +0200

    Package api in jar

--- chapter4/easytext-services/run-cli.sh
+++ chapter4/easytext-services/run-cli.sh
@@ -3,3 +3,4 @@ mkdir -p mods
 $JAVA_HOME/bin/javac --module-source-path src -d mods -m easytext.analysis.api,easytext.analysis.coleman,easytext.analysis.kincaid,easytext.cli
 
 $JAVA_HOME/bin/java --module-path mods -m easytext.cli/javamodularity.easytext.cli.Main ../exampletext.txt
+$JAVA_HOME/bin/java --module-path mods:mods/api.jar -m easytext.cli/javamodularity.easytext.cli.Main ../exampletext.txt
